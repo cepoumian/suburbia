8ea12bd35b2c25f90ba3174f038510d3476289aa
cepoumian <cepoumian@gmail.com>
2025-04-25 13:41:27 -0600

    Create interactive animated 3d skateboard

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,26 +1,40 @@
-import { FC } from "react"
-import { Content } from "@prismicio/client"
+import { FC } from "react";
+import { asImageSrc, Content } from "@prismicio/client";
 import {
   PrismicRichText,
   PrismicText,
   SliceComponentProps,
-} from "@prismicio/react"
-import { PrismicNextLink } from "@prismicio/next"
-import { Bounded } from "../components/Bounded"
-import { Heading } from "../components/Heading"
-import { ButtonLink } from "../components/ButtonLink"
-import { WideLogo } from "./WideLogo"
-import { TallLogo } from "./TallLogo"
+} from "@prismicio/react";
+// import { PrismicNextLink } from "@prismicio/next";
+import { Bounded } from "../components/Bounded";
+import { Heading } from "../components/Heading";
+import { ButtonLink } from "../components/ButtonLink";
+import { WideLogo } from "./WideLogo";
+import { TallLogo } from "./TallLogo";
+import { InteractiveSkateboard } from "./InteractiveSkateboard";
+
+const DEFAULT_DECK_TEXTURE = "/skateboard/Deck.webp";
+const DEFAULT_WHEEL_TEXTURE = "/skateboard/SkateWheel1.png";
+const DEFAULT_TRUCK_COLOR = "#6F6E6A";
+const DEFAULT_BOLT_COLOR = "#6F6E6A";
 
 /**
  * Props for `Hero`.
  */
-export type HeroProps = SliceComponentProps<Content.HeroSlice>
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 /**
  * Component for "Hero" Slices.
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
+  const deckTextureURL =
+    asImageSrc(slice.primary.skateboard_deck_texture) || DEFAULT_DECK_TEXTURE;
+  const wheelTextureURL =
+    asImageSrc(slice.primary.skateboard_wheel_texture) || DEFAULT_WHEEL_TEXTURE;
+  const truckColor =
+    slice.primary.skateboard_truck_color || DEFAULT_TRUCK_COLOR;
+  const boltColor = slice.primary.skateboard_bolt_color || DEFAULT_BOLT_COLOR;
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -39,19 +53,24 @@ const Hero: FC<HeroProps> = ({ slice }) => {
           <div className="max-w-[45ch] font-semibold ~text-lg/xl">
             <PrismicRichText field={slice.primary.body} />
           </div>
-          <PrismicNextLink field={slice.primary.button} />
+          <ButtonLink
+            field={slice.primary.button}
+            icon="skateboard"
+            size="lg"
+            className="z-20 mt-2 block"
+          >
+            {slice.primary.button.text}
+          </ButtonLink>
         </div>
-        <ButtonLink
-          field={slice.primary.button}
-          icon="skateboard"
-          size="lg"
-          className="z-20 mt-2 block"
-        >
-          {slice.primary.button.text}
-        </ButtonLink>
       </div>
+      <InteractiveSkateboard
+        deckTextureURL={deckTextureURL}
+        wheelTextureURL={wheelTextureURL}
+        truckColor={truckColor}
+        boltColor={boltColor}
+      />
     </Bounded>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
